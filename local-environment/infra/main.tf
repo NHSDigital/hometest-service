c40cd7d9ffa5729e5da5aa1c03bedae6c50edfab
lewisbirks <lewis.birks1@nhs.net>
2026-02-01 14:09:23 +0000

    refactor: restore url parity of existing test lambda
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>

--- local-environment/infra/main.tf
+++ local-environment/infra/main.tf
@@ -82,7 +82,7 @@ module "eligibility_test_info_lambda" {
   api_gateway_id                = aws_api_gateway_rest_api.api.id
   api_gateway_root_resource_id  = aws_api_gateway_rest_api.api.root_resource_id
   api_gateway_execution_arn     = aws_api_gateway_rest_api.api.execution_arn
-  api_path                      = "eligibility-test-info"
+  api_path                      = "test-order/info"
   lambda_role_policy_attachment = aws_iam_role_policy_attachment.lambda_basic
 
   environment_variables = {
