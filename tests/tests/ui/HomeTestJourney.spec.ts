3dbd565a6d00d34c03a39a23507f2f5661cd69b0
ConnorF-NHS <connor.forde1@nhs.net>
2026-02-02 10:24:34 +0000

    HOTE-523: Enter Address Manually (#28)
    * feat: initial setup + shared PageLayout component + start page
    
    * chore: add jest dependencies
    
    * test: unit tests for PageLayout component + jest config changes
    
    * feat: enter-deliver-address page
    
    * feat: enter delivery address screen
    
    * feat: initial state management
    
    * tests: enter-delivery-address tests
    
    * test: start page
    
    * chore: lint errors
    
    * chore: lint fix
    
    * home test start page
    
    * chore: update tests + remove comments
    
    * chore: remove prototype files + rename frontend to ui
    
    * test start page
    
    * chore: update README.md
    
    * updated comments
    
    * removed unused files
    
    * updated env
    
    * docs: add Pull Request Guidelines for best practices
    
    * refactor: update accessibility report directory path in AccessibilityModule
    
    * docs: enhance Pull Request Guidelines with additional checks and examples
    
    * chore: update text + add accessibility to urgent card
    
    * feat(HOTE-523): add manual address entry screen
    
    * feat: back button with navigation context/state
    
    * 427 Find address by postcode
    
    * resolved page load issues
    
    * resolved page load issues
    
    * refactor: move new files from /frontend to /ui
    
    * fix: merge error
    
    * refactor: rename HomeTestPage to HomeTestStartPage and update related fixtures
    
    * refactor: update accessibility check method to accept PageObject or Page
    
    * removed -ve scenarios
    
    * refactor: replace verifyPageLoaded with waitForPageLoaded and update tests; add new test files for HomeTestStartPage and OrderTestJourney
    
    * refactor: enhance HomeTestStartPage with external links and related tests; update configuration for new environment variables
    
    * comments are updated
    
    * renamed the file
    
    * renamed the method
    
    * renamed the method
    
    * refactor: update accessibility tests and improve page load handling
    
    * refactor: fix unit tests + remove unused code
    
    * refactor: lint issues
    
    * feat(HOTE-523): add back button to manual address entry page
    
    * refactor: lint issues
    
    * refactor: update accessibility tests and remove obsolete test files
    
    * refactor: update report directory handling in AccessibilityModule
    
    * Enter Address Manually
    
    * refactor: update variable names for consistency in accessibility tests
    
    * refactor: implement suggestions + fix how errors displayed on enter-delivery-address
    
    * feat: error summary
    
    * refactor: apply unit-tests patch
    
    * updated accessibility file
    
    * test: update unit tests to reflect error summary
    
    * refactor: remove href on error click
    
    * refactor: lint
    
    * refactor: add error summary + update unit tests + update postcode validation
    
    * refactor: lint
    
    * refactor: move enter-address-manually to new directory structure
    
    * refactor: rename waitForPageLoaded to waitUntilPageLoad for consistency
    
    ---------
    
    Co-authored-by: Cormac Feeney <cormac.feeney1@nhs.net>
    Co-authored-by: ravikumarqa28 <ravi.kumar28@nhs.net>
    Co-authored-by: Przemyslaw <przemyslaw.biesek1@nhs.net>

--- tests/tests/ui/HomeTestJourney.spec.ts
+++ tests/tests/ui/HomeTestJourney.spec.ts
@@ -4,17 +4,24 @@ import data from '../../test-data/address.json';
 
 test.describe.configure({ mode: 'serial' });
 
-test.describe('HIV Test Page', () => {
-  test.beforeEach(async ({ homeTestPage }) => {
-    await homeTestPage.navigate();
+test.describe('HIV Test Order journeys', () => {
+  test.beforeEach(async ({ homeTestStartPage }) => {
+    await homeTestStartPage.navigate();
   });
 
 
-  test('Order test journey', async ({ homeTestPage, findAddressPage }) => {
-    const actualResult = await homeTestPage.getHeaderText();
+  test('Order test journey', async ({ homeTestStartPage, findAddressPage }) => {
+    const actualResult = await homeTestStartPage.getHeaderText();
     expect(actualResult).toBe("Get a self-test kit for HIV");
-    await homeTestPage.clickStartNowButton();
+    await homeTestStartPage.clickStartNowButton();
     const randomEntry = data[Math.floor(Math.random() * data.length)];
-    await findAddressPage.fillPostCodeAndAddressAndContinue(randomEntry.postcode, randomEntry.address);
+    await findAddressPage.fillPostCodeAndAddressAndContinue(randomEntry.postcode, randomEntry.addressline1);
+  });
+
+  test('Order test journey by providing address manually', async ({ homeTestStartPage, findAddressPage, enterAddressManuallyPage }) => {
+    await homeTestStartPage.clickStartNowButton();
+    await findAddressPage.clickEnterAddressManuallyLink();
+    const randomEntry = data[Math.floor(Math.random() * data.length)];
+    await enterAddressManuallyPage.fillAddressAndContinue(randomEntry.addressline1, randomEntry.addressline2, randomEntry.addressline3, randomEntry.towncity, randomEntry.postcode)
   });
 });
