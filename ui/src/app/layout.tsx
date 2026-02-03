0ce3a7adc0d3a7b80c872c467dd21e909825f18f
Cormac-F-NHS <cormac.feeney1@nhs.net>
2026-02-03 14:26:24 +0000

    HOTE-537: No Address Found Page (#31)
    * feat: initial setup + shared PageLayout component + start page
    
    * chore: add jest dependencies
    
    * test: unit tests for PageLayout component + jest config changes
    
    * feat: enter-deliver-address page
    
    * feat: enter delivery address screen
    
    * feat: initial state management
    
    * tests: enter-delivery-address tests
    
    * test: start page
    
    * chore: lint errors
    
    * chore: lint fix
    
    * home test start page
    
    * chore: update tests + remove comments
    
    * chore: remove prototype files + rename frontend to ui
    
    * test start page
    
    * chore: update README.md
    
    * updated comments
    
    * removed unused files
    
    * updated env
    
    * docs: add Pull Request Guidelines for best practices
    
    * refactor: update accessibility report directory path in AccessibilityModule
    
    * docs: enhance Pull Request Guidelines with additional checks and examples
    
    * chore: update text + add accessibility to urgent card
    
    * feat(HOTE-523): add manual address entry screen
    
    * feat: back button with navigation context/state
    
    * 427 Find address by postcode
    
    * resolved page load issues
    
    * resolved page load issues
    
    * refactor: move new files from /frontend to /ui
    
    * fix: merge error
    
    * refactor: rename HomeTestPage to HomeTestStartPage and update related fixtures
    
    * refactor: update accessibility check method to accept PageObject or Page
    
    * removed -ve scenarios
    
    * refactor: replace verifyPageLoaded with waitForPageLoaded and update tests; add new test files for HomeTestStartPage and OrderTestJourney
    
    * refactor: enhance HomeTestStartPage with external links and related tests; update configuration for new environment variables
    
    * comments are updated
    
    * renamed the file
    
    * renamed the method
    
    * renamed the method
    
    * refactor: update accessibility tests and improve page load handling
    
    * refactor: fix unit tests + remove unused code
    
    * refactor: lint issues
    
    * feat(HOTE-523): add back button to manual address entry page
    
    * refactor: lint issues
    
    * refactor: update accessibility tests and remove obsolete test files
    
    * refactor: update report directory handling in AccessibilityModule
    
    * feat: no address found page
    
    * Enter Address Manually
    
    * refactor: update variable names for consistency in accessibility tests
    
    * refactor: implement suggestions + fix how errors displayed on enter-delivery-address
    
    * feat: error summary
    
    * refactor: apply unit-tests patch
    
    * updated accessibility file
    
    * test: update unit tests to reflect error summary
    
    * refactor: remove href on error click
    
    * refactor: lint
    
    * refactor: add error summary + update unit tests + update postcode validation
    
    * refactor: lint
    
    * refactor: lint
    
    * refactor: update page to reflect dir restructure + update config + update README.md
    
    * refactor: remove old file
    
    * Update README.md
    
    ---------
    
    Co-authored-by: ravikumarqa28 <ravi.kumar28@nhs.net>
    Co-authored-by: Przemyslaw <przemyslaw.biesek1@nhs.net>
    Co-authored-by: Connor Forde <connor.forde1@nhs.net>

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import 'nhsuk-frontend/dist/nhsuk.css';
-import { OrderProvider, NavigationProvider } from "@/state";
+import 'nhsuk-frontend/dist/nhsuk/nhsuk-frontend.css';
 
 export const metadata: Metadata = {
   title: "NHS HIV Home Test Service"
