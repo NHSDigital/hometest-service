a00b687ce3e61cc496afb66239f92a0c5ae2e320
rgolipally1 <rajesh.golipally1@nhs.net>
2026-02-04 11:31:39 +0000

    HOTE-562: assertion removed as per review comments

--- tests/tests/accessibility/OrderStatus_accessibility.spec.ts
+++ tests/tests/accessibility/OrderStatus_accessibility.spec.ts
@@ -6,7 +6,6 @@ test.describe('Accessibility Testing @accessibility', () => {
   test('Home Test - Confirmed Order Accessibility', async ({ orderStatusPage, accessibility }) => {
     await orderStatusPage.navigateToOrder('1');
     await orderStatusPage.waitUntilPageLoad();
-    await expect(orderStatusPage.orderHeading).toBeVisible();
 
     const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Confirmed Order");
     expect(hasViolations).toBe(false);
@@ -15,7 +14,6 @@ test.describe('Accessibility Testing @accessibility', () => {
   test('Home Test - Dispatched Order Accessibility', async ({ orderStatusPage, accessibility }) => {
     await orderStatusPage.navigateToOrder('2');
     await orderStatusPage.waitUntilPageLoad();
-    await expect(orderStatusPage.orderHeading).toBeVisible();
 
     const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Dispatched Order");
     expect(hasViolations).toBe(false);
@@ -24,7 +22,6 @@ test.describe('Accessibility Testing @accessibility', () => {
   test('Home Test - Test Received Order Accessibility', async ({ orderStatusPage, accessibility }) => {
     await orderStatusPage .navigateToOrder('3');
     await orderStatusPage.waitUntilPageLoad();
-    await expect(orderStatusPage.orderHeading).toBeVisible();
 
     const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Test Received");
     expect(hasViolations).toBe(false);
@@ -33,7 +30,6 @@ test.describe('Accessibility Testing @accessibility', () => {
   test('Home Test - Your results is ready Accessibility', async ({ orderStatusPage, accessibility }) => {
     await orderStatusPage.navigateToOrder('4');
     await orderStatusPage.waitUntilPageLoad();
-    await expect(orderStatusPage.orderHeading).toBeVisible();
 
     const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Your results is ready");
     expect(hasViolations).toBe(false);
