fbdfa69a29df8919f2148eedd00504af887914d0
Przemyslaw <przemyslaw.biesek1@nhs.net>
2026-02-06 10:19:14 +0100

    refactor: update accessibility checks to return error arrays and improve report generation

--- tests/tests/accessibility/OrderStatus_accessibility.spec.ts
+++ tests/tests/accessibility/OrderStatus_accessibility.spec.ts
@@ -1,38 +1,27 @@
 import { expect, Page } from '@playwright/test';
 import { test } from '../../fixtures';
+import { type Result } from 'axe-core';
 
+const accessErrors: Result[] = [];
 test.describe('Accessibility Testing @accessibility', () => {
 
   test('Home Test - Confirmed Order Accessibility', async ({ orderStatusPage, accessibility }) => {
     await orderStatusPage.navigateToOrder('1');
     await orderStatusPage.waitUntilPageLoad();
-
-    const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Confirmed Order");
-    expect(hasViolations).toBe(false);
-  });
-
-  test('Home Test - Dispatched Order Accessibility', async ({ orderStatusPage, accessibility }) => {
+    accessErrors.push(...await accessibility.runAccessibilityCheck(orderStatusPage, "Status Confirmed", "Order Tracking Page"));
+   
     await orderStatusPage.navigateToOrder('2');
     await orderStatusPage.waitUntilPageLoad();
-
-    const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Dispatched Order");
-    expect(hasViolations).toBe(false);
-  });
-
-  test('Home Test - Test Received Order Accessibility', async ({ orderStatusPage, accessibility }) => {
-    await orderStatusPage .navigateToOrder('3');
+    accessErrors.push(...await accessibility.runAccessibilityCheck(orderStatusPage, "Status Dispatched", "Order Tracking Page"));
+    
+    await orderStatusPage.navigateToOrder('3');
     await orderStatusPage.waitUntilPageLoad();
-
-    const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Test Received");
-    expect(hasViolations).toBe(false);
-  });
-
-  test('Home Test - Your results is ready Accessibility', async ({ orderStatusPage, accessibility }) => {
+    accessErrors.push(...await accessibility.runAccessibilityCheck(orderStatusPage, "Status Test received", "Order Tracking Page"));
+    
     await orderStatusPage.navigateToOrder('4');
     await orderStatusPage.waitUntilPageLoad();
-
-    const hasViolations = await accessibility.runAccessibilityCheck(orderStatusPage, "HIV self-test - Your results is ready");
-    expect(hasViolations).toBe(false);
+    accessErrors.push(...await accessibility.runAccessibilityCheck(orderStatusPage, "Status Result Ready", "Order Tracking Page"));
+    
+    expect(accessErrors).toHaveLength(0);
   });
-
 });
