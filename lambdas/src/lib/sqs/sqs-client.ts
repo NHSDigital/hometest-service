6e972f66c0111612c7fc2ceef53cf96871a87b5c
Thomas Seelig <20642658+TSeelig@users.noreply.github.com>
2026-02-12 10:24:00 +0000

    Story/hote 520 test result submit (#64)
    * HOTE-520: order-result-lambda
    
    * add order-result-lambda and Amazon SQS message queue to localStack for localdev
    
    * chore fix merge conflict issue
    
    * add some checks to better handle empty request bodies
    add some code to clean up error output to strip unnessesary decoration added by zod.prettify
    
    * unit tests for order-result-lambda & sqs-client
    
    * chore: clean up semantic issues like trailing whitespace
    
    * chore: clean up TODO
    
    * chore: semantic changes
    
    * chore: semantic changes
    
    * remove unused exported function from sqs-client
    
    * HOTE-520: API Automation Added and validated successfully
    
    * HOTE-520: addressed review comments
    
    * HOTE-520: local environment updated
    
    * modify response to echo request body
    
    * chore package-lock
    
    * small update to validation on basedOn to allow child properties other than reference
    
    * switch expect interpretation to expecting valueCodeableConcept for result
    
    * HOTE-520: playwright config updated
    
    ---------
    
    Co-authored-by: rgolipally1 <rajesh.golipally1@nhs.net>
    Co-authored-by: rgolipally1 <253470777+rgolipally1@users.noreply.github.com>

---
+++ lambdas/src/lib/sqs/sqs-client.ts
@@ -0,0 +1,39 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+
+export interface SqsResult {
+  messageId?: string;
+  sequenceNumber?: string;
+}
+
+export interface SQSClientInterface {
+  sendMessage(queueUrl: string, messageBody: string, messageAttributes?: Record<string, any>): Promise<SqsResult>;
+}
+export class AWSSQSClient implements SQSClientInterface {
+  private client: SQSClient;
+
+  constructor(region: string = process.env.AWS_REGION || "eu-west-1") {
+    this.client = new SQSClient({
+      region,
+      maxAttempts: 3,
+      endpoint: process.env.SQS_ENDPOINT,
+    });
+  }
+
+  async sendMessage(
+    queueUrl: string,
+    messageBody: string,
+    messageAttributes?: Record<string, any>
+  ): Promise<SqsResult> {
+    const command = new SendMessageCommand({
+      QueueUrl: queueUrl,
+      MessageBody: messageBody,
+      MessageAttributes: messageAttributes,
+    });
+
+    const result = await this.client.send(command);
+    return {
+      messageId: result.MessageId,
+      sequenceNumber: result.SequenceNumber,
+    };
+  }
+}
