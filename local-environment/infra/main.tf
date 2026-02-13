4b2fc6f30145cdc162be1eaab5c2cff9a77aa100
Bill Wirz <bill.wirz1@nhs.net>
2026-02-13 11:56:05 +0000

    feat/hote-633-sqs-order-router
    * feat: add-zod-schema-validation-for-fhir-service-request
    
    * feat: create-localstack-queue-and-make-esm-for-order-router
    
    * chore: extract-out-zod-schemas
    
    * chore: create-fhir-service-request-type
    
    * chore: refactor-to-throw-for-missing-correlation-id
    
    * feat: refactor-order-router-to-read-sqs-message
    
    * chore: improve-error-handling-and-structure
    
    * chore: move-mocks
    
    * chore: rename-schema-name
    
    * chore: remove-order-router-from-api-gw

--- local-environment/infra/main.tf
+++ local-environment/infra/main.tf
@@ -176,6 +176,10 @@ module "hello_world_lambda" {
   }
 }
 
+resource "aws_sqs_queue" "order_placement" {
+  name = "${var.project_name}-order-placement"
+}
+
 module "order_router_lambda" {
   source = "./modules/lambda"
 
@@ -199,6 +203,13 @@ module "order_router_lambda" {
     SUPPLIER_CLIENT_ID          = "supplier-client"
     SUPPLIER_CLIENT_SECRET_NAME = "supplier-oauth-client-secret"
   }
+}
+
+resource "aws_lambda_event_source_mapping" "order_router_order_placement" {
+  event_source_arn = aws_sqs_queue.order_placement.arn
+  function_name    = module.order_router_lambda.lambda_function.arn
+  enabled          = true
+  batch_size       = 1
 }
 
 # SQS Queue for order results
@@ -235,7 +246,6 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 
   depends_on = [
     module.eligibility_test_info_lambda,
-    module.order_router_lambda,
     module.order_result_lambda,
     module.login_lambda,
   ]
