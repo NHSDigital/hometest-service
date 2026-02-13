a611f12429631e22e07f80c79ba5b0d57ab49b49
ravikumarqa28 <ravi.kumar28@nhs.net>
2026-02-13 14:22:34 +0000

    privary policy comments

--- tests/page-objects/HomeTestStartPage.ts
+++ tests/page-objects/HomeTestStartPage.ts
@@ -9,7 +9,7 @@ export class HomeTestStartPage extends BasePage {
   readonly nearestSexualHealthClinicLink: Locator;
   readonly learnMoreHIVAidsLink: Locator;
   readonly startNowBtn: Locator;
-  readonly privacyPolicy: Locator;
+  readonly privacyPolicyLink: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -19,7 +19,7 @@ export class HomeTestStartPage extends BasePage {
     this.learnMoreHIVAidsLink = page.getByRole('link', { name: 'Learn more about HIV and AIDS' });
     this.startNowBtn = page.getByRole('button', { name: 'Start now' });
     this.config = ConfigFactory.getConfig();
-    this.privacyPolicy = page.getByRole('link', { name: 'privacy policy' });
+    this.privacyPolicyLink = page.getByRole('link', { name: 'privacy policy' });
   }
   async navigate(): Promise<void> {
     await this.page.goto(`${this.config.uiBaseUrl}/get-self-test-kit-for-HIV`);
@@ -50,7 +50,7 @@ export class HomeTestStartPage extends BasePage {
   }
 
   async clickPrivacyPolicyLink(): Promise<void> {
-    await this.privacyPolicy.click();
+    await this.privacyPolicyLink.click();
   }
 
 }
