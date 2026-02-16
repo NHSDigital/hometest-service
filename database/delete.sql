8da7bb26ea1e298abc8c357384b71dea8f597549
Mikolaj Miotk <mikolaj.miotk2@nhs.net>
2026-02-16 14:19:04 +0100

    add lambdas eslint

--- database/delete.sql
+++ database/delete.sql
@@ -1,8 +1,9 @@
 -- Terminate existing connections to allow drop
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'local_hometest_db'
-  AND pid <> pg_backend_pid();
+WHERE
+    datname = 'local_hometest_db'
+    AND pid <> pg_backend_pid();
 
 DROP DATABASE IF EXISTS local_hometest_db;
 CREATE DATABASE local_hometest_db;
