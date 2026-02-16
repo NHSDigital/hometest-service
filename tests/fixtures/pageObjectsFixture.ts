8792df6dc670604ac2da0d14071c41f7d7d3e4ba
ravikumarqa28 <ravi.kumar28@nhs.net>
2026-02-16 09:14:20 +0000

    removed spaces

--- tests/fixtures/pageObjectsFixture.ts
+++ tests/fixtures/pageObjectsFixture.ts
@@ -1,5 +1,5 @@
 import { test as base } from '@playwright/test';
-import { FindAddressPage, HomeTestStartPage, EnterAddressManuallyPage, SelectDeliveryAddressPage, OrderStatusPage, HowComfortablePrickingFingerPage,PrivacyPolicyPage } from '../page-objects';
+import { FindAddressPage, HomeTestStartPage, EnterAddressManuallyPage, SelectDeliveryAddressPage, OrderStatusPage, HowComfortablePrickingFingerPage, PrivacyPolicyPage } from '../page-objects';
 
 export interface MyFixtures {
   homeTestStartPage: HomeTestStartPage;
