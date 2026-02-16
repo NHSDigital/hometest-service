7c86728ed4d1bae1204de9f93c98337c30fbef8c
Mikolaj Miotk <mikolaj.miotk2@nhs.net>
2026-02-16 14:21:53 +0100

    add lambdas eslint v2

--- database/delete.sql
+++ database/delete.sql
@@ -2,8 +2,8 @@
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
 WHERE
-    datname = 'local_hometest_db'
-    AND pid <> pg_backend_pid();
+  datname = 'local_hometest_db'
+  AND pid <> pg_backend_pid();
 
 DROP DATABASE IF EXISTS local_hometest_db;
 CREATE DATABASE local_hometest_db;
--- database/seed.sql
+++ database/seed.sql
@@ -1,23 +1,23 @@
 SET search_path TO hometest;
 
 INSERT INTO supplier (
-    name,
-    service_url,
-    website_url,
-    client_secret_name,
-    client_id,
-    oauth_token_path,
-    order_path,
-    oauth_scope
+  name,
+  service_url,
+  website_url,
+  client_secret_name,
+  client_id,
+  oauth_token_path,
+  order_path,
+  oauth_scope
 )
 VALUES (
-    'Test Supplier',
-    'http://wiremock:8080',
-    'http://example.com',
-    'test_supplier_client_secret',
-    'test_supplier_client_id',
-    '/oauth/token',
-    '/order',
-    'orders results'
+  'Test Supplier',
+  'http://wiremock:8080',
+  'http://example.com',
+  'test_supplier_client_secret',
+  'test_supplier_client_id',
+  '/oauth/token',
+  '/order',
+  'orders results'
 )
 ON CONFLICT DO NOTHING;
