0b76e8a6d02722fd9f572948aeca0a39cc2bcb05
Naidu167 <apparao.naidukarri1@nhs.net>
2026-02-18 16:15:50 +0000

    HOTE-538 Enter mobile number

---
+++ tests/page-objects/EnterMobileNumberPage.ts
@@ -0,0 +1,23 @@
+import { Locator, Page} from '@playwright/test';
+import { ConfigFactory, type ConfigInterface } from '../configuration/configuration';
+import { BasePage } from './BasePage';
+import { AddressModel } from '../models';
+
+export class EnterMobileNumberPage extends BasePage 
+{
+  readonly mobileNumberInput: Locator;  
+  readonly continueButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.mobileNumberInput = page.locator('#mobile-number');
+    this.continueButton = page.getByRole('button', { name: 'Continue' });
+  }
+
+  async fillMobileNumberAndContinue(randomEntry: AddressModel): Promise<void> {
+    await this.mobileNumberInput.fill(randomEntry.mobileNumber);        
+    await this.continueButton.click();
+  }
+}
+  
+
