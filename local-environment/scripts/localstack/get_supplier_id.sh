885c2a205ea991b38bc5b9e23ddee6ef79db07d8
Mikolaj Miotk <mikolaj.miotk2@nhs.net>
2026-02-18 16:18:06 +0100

    add some checks

--- local-environment/scripts/localstack/get_supplier_id.sh
+++ local-environment/scripts/localstack/get_supplier_id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-for i in {1..10}; do
+for _ in {1..10}; do
   SUPPLIER_ID=$(docker exec postgres-db psql "postgresql://app_user:STRONG_APP_PASSWORD@localhost:5432/local_hometest_db" -A -t -c "SET search_path TO hometest; SELECT supplier_id FROM supplier LIMIT 1;" 2>/dev/null | grep -v '^$' | grep -v '^SET$' | head -n 1 || echo "")
   if [[ -n "$SUPPLIER_ID" ]]; then
     echo "{\"supplier_id\": \"$SUPPLIER_ID\"}"
