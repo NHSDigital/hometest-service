5886b5171f6beb2f7434d272d604a5b10a236853
Naidu167 <apparao.naidukarri1@nhs.net>
2026-02-19 13:50:01 +0000

    accessibility changes for enter mobile number

--- tests/tests/accessibility/Enter_mobile_number_accessibility.spec.ts
+++ tests/tests/accessibility/Enter_mobile_number_accessibility.spec.ts
@@ -4,14 +4,17 @@ import { AddressModel } from '../../models';
 
 test.describe('Accessibility Testing @accessibility', () => {
 
-  test('Select Delivery Address Page', async ({ homeTestStartPage, findAddressPage, selectDeliveryAddressPage, accessibility, enterMobileNumberPage }) => {
+  test('Select Delivery Address Page', async ({ homeTestStartPage, findAddressPage, selectDeliveryAddressPage, accessibility, enterMobileNumberPage, howComfortablePrickingFingerPage }) => {
     await homeTestStartPage.navigate();
     await homeTestStartPage.clickStartNowButton();
     const randomAddress = AddressModel.getRandomAddress();
     await findAddressPage.fillPostCodeAndAddressAndContinue(randomAddress);
     await selectDeliveryAddressPage.waitUntilPageLoad();
+    await selectDeliveryAddressPage.selectAddressAndContinue();
+    await howComfortablePrickingFingerPage.waitUntilPageLoad();
+    await howComfortablePrickingFingerPage.selectYesOptionAndContinue();      
     await enterMobileNumberPage.fillMobileNumberAndContinue(randomAddress);
-    const accessErrors = await accessibility.runAccessibilityCheck(enterMobileNumberPage.page, "Enter Mobile Number Page");
-    expect(accessErrors).toHaveLength(0);
+    const accessErrors2 = await accessibility.runAccessibilityCheck(enterMobileNumberPage.page, "Enter Mobile Number Page");
+    expect(accessErrors2).toHaveLength(0);
   });
 });
