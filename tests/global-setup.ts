e134b8420236d248f2cf01e1003d60aabcdd4b8a
krssw <krzysztof.swiatek3@nhs.net>
2026-02-20 09:38:34 +0100

    Remove depricit code

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -2,10 +2,6 @@ import { CredentialsHelper } from './utils/CredentialsHelper';
 import { UserManagerFactory } from './utils/users/UserManagerFactory';
 
 async function globalSetup() {
-  if (process.env.DB_ONLY === 'true') {
-    console.log('Skipping global login for DB-only tests');
-    return;
-  }
   console.log('🚀 Global setup started');
   console.log(`Tests will run on environment: ${process.env.ENV ?? 'local'}`);
 
