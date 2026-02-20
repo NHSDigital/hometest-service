becdf80459bcad3d3aef4ce4a7e79a440e4fc860
Naidu167 <apparao.naidukarri1@nhs.net>
2026-02-20 13:59:47 +0000

    updated file

--- tests/tests/accessibility/Enter_mobile_number_accessibility.spec.ts
+++ tests/tests/accessibility/Enter_mobile_number_accessibility.spec.ts
@@ -5,18 +5,18 @@ import { PersonalDetailsModel } from '../../models/PersonalDetails';
 
 test.describe('Accessibility Testing @accessibility', () => {
 
-  test('Enter mobile number scenario', async ({ homeTestStartPage, findAddressPage, selectDeliveryAddressPage, accessibility, enterMobileNumberPage, howComfortablePrickingFingerPage }) => {
-    await homeTestStartPage.navigate();
-    await homeTestStartPage.clickStartNowButton();
-    const randomAddress = AddressModel.getRandomAddress();
-    await findAddressPage.fillPostCodeAndAddressAndContinue(randomAddress);
-    await selectDeliveryAddressPage.waitUntilPageLoad();
-    await selectDeliveryAddressPage.selectAddressAndContinue();
-    await howComfortablePrickingFingerPage.waitUntilPageLoad();
-    await howComfortablePrickingFingerPage.selectYesOptionAndContinue();
-    const randomMobileNumber = PersonalDetailsModel.getRandomPersonalDetails();
-    await enterMobileNumberPage.fillMobileNumberAndContinue(randomMobileNumber);
-    const accessErrors = await accessibility.runAccessibilityCheck(enterMobileNumberPage.page, "Enter Mobile Number Page");
-    expect(accessErrors).toHaveLength(0);
-  });
+    test('Enter mobile number scenario', async ({ homeTestStartPage, findAddressPage, selectDeliveryAddressPage, accessibility, enterMobileNumberPage, howComfortablePrickingFingerPage }) => {
+        await homeTestStartPage.navigate();
+        await homeTestStartPage.clickStartNowButton();
+        const randomAddress = AddressModel.getRandomAddress();
+        await findAddressPage.fillPostCodeAndAddressAndContinue(randomAddress);
+        await selectDeliveryAddressPage.waitUntilPageLoad();
+        await selectDeliveryAddressPage.selectAddressAndContinue();
+        await howComfortablePrickingFingerPage.waitUntilPageLoad();
+        await howComfortablePrickingFingerPage.selectYesOptionAndContinue();
+        const randomMobileNumber = PersonalDetailsModel.getRandomPersonalDetails();
+        await enterMobileNumberPage.fillMobileNumberAndContinue(randomMobileNumber);
+        const accessErrors = await accessibility.runAccessibilityCheck(enterMobileNumberPage.page, "Enter Mobile Number Page");
+        expect(accessErrors).toHaveLength(0);
+    });
 });
