73a1a1361e98510a8fa56d9cfaec7c2e817384a8
billwirz1 <bill.wirz1@nhs.net>
2026-02-20 15:47:22 +0000

    fix: change-order-status-migration

--- database/migrations/000003_unique_order_status_per_order.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE order_status
-ADD CONSTRAINT unique_order_status_per_order UNIQUE (order_uid);
-
--- +goose Down
-ALTER TABLE order_status
-DROP CONSTRAINT IF EXISTS unique_order_status_per_order;
