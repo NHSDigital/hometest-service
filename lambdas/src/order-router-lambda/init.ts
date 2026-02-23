dc40a4e1df8b7aa73b85ec88d84951f1f20d5594
billwirz1 <bill.wirz1@nhs.net>
2026-02-23 12:41:20 +0000

    feat: update-order-router-lambda-to-use-db-env-vars

--- lambdas/src/order-router-lambda/init.ts
+++ lambdas/src/order-router-lambda/init.ts
@@ -2,6 +2,7 @@ import { FetchHttpClient } from "../lib/http/http-client";
 import { SupplierService } from "../lib/db/supplier-db";
 import { PostgresDbClient } from "../lib/db/db-client";
 import { AwsSecretsClient } from "../lib/secrets/secrets-manager-client";
+import { retrieveMandatoryEnvVariable } from "../lib/utils";
 
 export interface Environment {
   httpClient: FetchHttpClient;
@@ -10,14 +11,23 @@ export interface Environment {
 }
 
 export function init(): Environment {
-  const databaseUrl = process.env.DATABASE_URL!;
   const awsRegion =
     process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || "eu-west-2";
 
   const httpClient = new FetchHttpClient();
-  const dbClient = new PostgresDbClient(databaseUrl);
-  const supplierDb = new SupplierService({ dbClient });
   const secretsClient = new AwsSecretsClient(awsRegion);
+  const dbClient = new PostgresDbClient(
+    {
+      username: retrieveMandatoryEnvVariable("DB_USERNAME"),
+      address: retrieveMandatoryEnvVariable("DB_ADDRESS"),
+      port: retrieveMandatoryEnvVariable("DB_PORT"),
+      database: retrieveMandatoryEnvVariable("DB_NAME"),
+      schema: retrieveMandatoryEnvVariable("DB_SCHEMA"),
+      passwordSecretName: retrieveMandatoryEnvVariable("DB_SECRET_NAME"),
+    },
+    secretsClient,
+  );
+  const supplierDb = new SupplierService({ dbClient });
 
   return {
     httpClient,
