094abfad5b5dfeaaa35f34355152ebeb11a5e03d
billwirz1 <bill.wirz1@nhs.net>
2026-02-23 12:40:48 +0000

    feat: update-eligibility-lambda-to-use-db-env-vars

--- lambdas/src/eligibility-lookup-lambda/init.ts
+++ lambdas/src/eligibility-lookup-lambda/init.ts
@@ -1,24 +1,39 @@
-import {Commons, ConsoleCommons} from "../lib/commons";
-import {SupplierService} from "../lib/db/supplier-db";
-import {LaLookupService} from "./la-lookup";
-import {PostgresDbClient} from "../lib/db/db-client";
+import { Commons, ConsoleCommons } from "../lib/commons";
+import { SupplierService } from "../lib/db/supplier-db";
+import { LaLookupService } from "./la-lookup";
+import { PostgresDbClient } from "../lib/db/db-client";
+import { AwsSecretsClient } from "../lib/secrets/secrets-manager-client";
+import { retrieveMandatoryEnvVariable } from "../lib/utils";
 
 
 export interface Environment {
   commons: Commons;
   supplierDb: SupplierService;
-  laLookupService: LaLookupService
+  laLookupService: LaLookupService;
 }
 
 export function init(): Environment {
   const commons = new ConsoleCommons();
-  const dbClient = new PostgresDbClient(process.env.DATABASE_URL!);
+  const awsRegion =
+    process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || "eu-west-2";
+  const secretsClient = new AwsSecretsClient(awsRegion);
+  const dbClient = new PostgresDbClient(
+    {
+      username: retrieveMandatoryEnvVariable("DB_USERNAME"),
+      address: retrieveMandatoryEnvVariable("DB_ADDRESS"),
+      port: retrieveMandatoryEnvVariable("DB_PORT"),
+      database: retrieveMandatoryEnvVariable("DB_NAME"),
+      schema: retrieveMandatoryEnvVariable("DB_SCHEMA"),
+      passwordSecretName: retrieveMandatoryEnvVariable("DB_SECRET_NAME"),
+    },
+    secretsClient,
+  );
   const supplierDb = new SupplierService({ dbClient });
   const laLookupService = new LaLookupService();
 
   return {
     commons,
     supplierDb,
-    laLookupService
+    laLookupService,
   };
 }
