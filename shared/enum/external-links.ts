8d449b5f89eb25c3a22f0763fd3a12e97b09f535
krssw <krzysztof.swiatek3@nhs.net>
2026-02-23 11:46:36 +0100

    Exported consts to an enum and cleaned up the code, extended gitignore to stop env files from appearing

---
+++ shared/enum/external-links.ts
@@ -0,0 +1,5 @@
+export const EXTERNAL_LINKS = {
+  SEXUAL_HEALTH_CLINIC: 'https://www.nhs.uk/service-search/sexual-health-services/find-a-sexual-health-clinic/',
+  NEAREST_AE: 'https://www.nhs.uk/service-search/find-an-accident-and-emergency-service/',
+  HIV_AIDS_INFO: 'https://www.nhs.uk/conditions/hiv-and-aids/',
+};
