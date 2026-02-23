e37989a53b54dea901ec36283e434f4a71055f35
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-02-23 17:43:23 +0100

    HOTE-611: Home test consumer API - GET /order (to reflect status in UI) (#85)
    * feat(HOTE-611): init implementation of new lambda
    
    * feat(HOTE-611): pull status description
    
    * feat: set-up-goose-for-db-migrations
    
    * chore: set-up-scripts-and-readme-for-migrations
    
    * fix: missing-newlines
    
    * fix: missing-newlines
    
    * feat: add-seed-data-with-goose
    
    * fix: eof-errors
    
    * fix: remove-trailing-whitespace
    
    * chore: add-eof-to-pre-commit
    
    * chore: remove-keywords-from-schemas
    
    * feat: reafactor code
    
    * fix: swap-static-and-seed-data-from-migrations
    
    * feat: added unit tests
    
    * chore: line endings
    
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>
    
    * fix: broken-foreign-key
    
    * fix: comment-out-delete-sql
    
    * feat: refactoring and tests
    
    * fix: correct seed script
    
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>
    
    * feat: update local terraform with new lambda
    
    * fix: seed-data-errors
    
    * fix: remove-unused-pre-commit-hook
    
    * fix: fix after tests
    
    * fix: added cors configuration and replaced hardcoded patient data
    
    * fix: correct frommating
    
    * fix: fix ui test
    
    * fix: fix lint error
    
    * fix: corrections after review
    
    * fix: updated redeployment trigger
    
    * fix: return the test type from database
    
    * Update Order status page accessibility tests, add Get order api tests, add db methods
    
    * fix: fix tests
    
    ---------
    
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>
    Co-authored-by: billwirz1 <bill.wirz1@nhs.net>
    Co-authored-by: lewisbirks <lewis.birks1@nhs.net>
    Co-authored-by: Masha-kainos <256341953+Masha-kainos@users.noreply.github.com>

---
+++ lambdas/src/lib/db/order-db-client.ts
@@ -0,0 +1,63 @@
+import { type DBClient } from "./db-client";
+
+type OrderStatusCode =
+  | "ORDER_RECEIVED"
+  | "DISPATCHED"
+  | "RECEIVED"
+  | "COMPLETE";
+
+export interface Order {
+  id: string;
+  reference_number: number;
+  created_at: Date;
+  test_code: string;
+  test_description: string;
+  status_code: OrderStatusCode;
+  status_description: string;
+  status_created_at: Date;
+  supplier_id: string;
+  supplier_name: string;
+  patient_nhs_number: string;
+  patient_birth_date: Date;
+}
+
+export class OrderDbClient {
+  readonly dbClient: DBClient;
+  constructor(dbClient: DBClient) {
+    this.dbClient = dbClient;
+  }
+
+  public async getOrder(orderId: string, nhsNumber: string, dateOfBirth: Date) {
+    const query = `
+      SELECT
+          o.order_uid AS id,
+          o.order_reference AS reference_number,
+          o.created_at AS created_at,
+          o.test_code AS test_code,
+          tt.description AS test_description,
+          os.status_code AS status_code,
+          st.description AS status_description,
+          os.created_at AS status_created_at,
+          s.supplier_id AS supplier_id,
+          s.supplier_name AS supplier_name,
+          p.nhs_number AS patient_nhs_number,
+          p.birth_date AS patient_birth_date
+      FROM hometest.test_order o
+      INNER JOIN test_type tt ON tt.test_code = o.test_code
+      INNER JOIN hometest.order_status os ON os.order_uid = o.order_uid
+      INNER JOIN hometest.status_type st ON st.status_code = os.status_code
+      INNER JOIN hometest.patient_mapping p ON p.patient_uid = o.patient_uid
+      INNER JOIN hometest.supplier s ON s.supplier_id = o.supplier_id
+      WHERE o.order_uid = $1 AND p.nhs_number = $2 AND p.birth_date = $3::date
+      ORDER BY os.created_at DESC
+      LIMIT 1;
+    `;
+
+    const result = await this.dbClient.query<Order, [string, string, Date]>(
+      query,
+      [orderId, nhsNumber, dateOfBirth],
+    );
+
+    return result?.rows[0] ?? null;
+  }
+}
