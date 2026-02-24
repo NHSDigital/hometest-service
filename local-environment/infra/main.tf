7a631f9cd88a9417dd6a54a671fb3b7b91cae42a
Usmaan Ali <usmaan.ali5@nhs.net>
2026-02-24 10:30:34 +0000

    chore: pr comment, add to triggers

--- local-environment/infra/main.tf
+++ local-environment/infra/main.tf
@@ -416,6 +416,8 @@ resource "aws_api_gateway_deployment" "api_deployment" {
       module.login_lambda,
       module.order_service_lambda,
       module.session_lambda
+      module.session_lambda,
+      module.order_status_lambda,
     ]))
   }
 
