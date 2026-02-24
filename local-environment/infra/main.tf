4b0a91ce7e3d312fe7ec841325dd0e5fd384914c
Usmaan Ali <usmaan.ali5@nhs.net>
2026-02-24 11:37:08 +0000

    chore: rebase terraform fix

--- local-environment/infra/main.tf
+++ local-environment/infra/main.tf
@@ -415,7 +415,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
       module.get_order_lambda,
       module.login_lambda,
       module.order_service_lambda,
-      module.session_lambda
+      module.session_lambda,
       module.session_lambda,
       module.order_status_lambda,
     ]))
