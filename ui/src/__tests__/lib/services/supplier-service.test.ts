e69428452733341c0acda10130ead0999a5f794d
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-02-26 17:41:04 +0100

    fix: refactor test

--- ui/src/__tests__/lib/services/supplier-service.test.ts
+++ ui/src/__tests__/lib/services/supplier-service.test.ts
@@ -1,39 +1,42 @@
 import supplierService from "@/lib/services/supplier-service";
 
 describe("SupplierService", () => {
-  it("returns links for Preventx", () => {
-    const result = supplierService.getLinksBySupplierName("Preventx");
+  it.each([
+    {
+      testName: "returns links for Preventx",
+      supplierName: "Preventx",
+      expected: {
+        sexualHealth: "https://www.sh.uk/",
+        contact: "https://www.sh.uk/contact-us",
+      },
+    },
+    {
+      testName: "returns links for SH24",
+      supplierName: "SH24",
+      expected: {
+        sexualHealth: "https://sh24.org.uk/",
+        contact: "https://freetesting.hiv/contact",
+      },
+    },
+    {
+      testName: "matches supplier names case-insensitively",
+      supplierName: "  pReVeNtX  ",
+      expected: {
+        sexualHealth: "https://www.sh.uk/",
+        contact: "https://www.sh.uk/contact-us",
+      },
+    },
+    {
+      testName: "returns empty links for unknown supplier",
+      supplierName: "Unknown Supplier",
+      expected: {
+        sexualHealth: "#",
+        contact: "#",
+      },
+    },
+  ])("$testName", ({ supplierName, expected }) => {
+    const result = supplierService.getLinksBySupplierName(supplierName);
 
-    expect(result).toEqual({
-      sexualHealth: "https://www.sh.uk/",
-      contact: "https://www.sh.uk/contact-us",
-    });
-  });
-
-  it("returns links for SH24", () => {
-    const result = supplierService.getLinksBySupplierName("SH24");
-
-    expect(result).toEqual({
-      sexualHealth: "https://sh24.org.uk/",
-      contact: "https://freetesting.hiv/contact",
-    });
-  });
-
-  it("matches supplier names case-insensitively", () => {
-    const result = supplierService.getLinksBySupplierName("  pReVeNtX  ");
-
-    expect(result).toEqual({
-      sexualHealth: "https://www.sh.uk/",
-      contact: "https://www.sh.uk/contact-us",
-    });
-  });
-
-  it("returns empty links for unknown supplier", () => {
-    const result = supplierService.getLinksBySupplierName("Unknown Supplier");
-
-    expect(result).toEqual({
-      sexualHealth: "#",
-      contact: "#",
-    });
+    expect(result).toEqual(expected);
   });
 });
