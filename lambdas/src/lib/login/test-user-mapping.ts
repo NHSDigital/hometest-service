fda7356012601698d078f5a1691fafc750b0ac65
Cormac Feeney <cormac.feeney1@nhs.net>
2026-02-26 15:28:18 +0000

    refactor: move name enrichment method to test-user-mapping.ts

--- lambdas/src/lib/login/test-user-mapping.ts
+++ lambdas/src/lib/login/test-user-mapping.ts
@@ -1,3 +1,5 @@
+import { type INhsUserInfoResponseModel } from '../models/nhs-login/nhs-login-user-info-response-model';
+
 /**
  * Test user first names lookup (temporary workaround for missing scope in NHS Login)
  * Maps family name (surname) to given name (first name) for known test users
@@ -28,3 +30,25 @@ export const TEST_FIRST_NAMES: Record<string, string> = {
   PRYDE: 'Toni',
   'KELSO-CID': 'Huberto',
 };
+
+/**
+ * Enriches user info with test first names for known test users (temporary workaround)
+ * Fills in missing given_name based on family_name lookup
+ * ALPHA: TODO: Remove this when the given_name (profile_extended) scope is available from NHS Login
+ * @param userInfo - The user info response from NHS Login
+ * @returns Enriched user info with given_name populated if missing
+ */
+export function enrichUserInfoWithTestFirstName(
+  userInfo: INhsUserInfoResponseModel
+): INhsUserInfoResponseModel {
+  if (userInfo.given_name) {
+    return userInfo;
+  }
+
+  const testFirstName = TEST_FIRST_NAMES[userInfo.family_name];
+  if (testFirstName) {
+    return { ...userInfo, given_name: testFirstName };
+  }
+
+  return userInfo;
+}
