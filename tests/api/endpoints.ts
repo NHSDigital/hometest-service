f7513d4fd3590f3239d2760cf1cae2f43c881f0a
krssw <krzysztof.swiatek3@nhs.net>
2026-02-27 15:41:52 +0100

    Fix typos

--- tests/api/endpoints.ts
+++ tests/api/endpoints.ts
@@ -1,10 +1,10 @@
 export const API_ENDPOINTS = {
   results: {
-    base: '/result',
-    get: '/results',
+    base: "/result",
+    get: "/results",
   },
   order: {
-    create: '/order',
-    get: '/order',
+    create: "/order",
+    get: "/order",
   },
 } as const;
