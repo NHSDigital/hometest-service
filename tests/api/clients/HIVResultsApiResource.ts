6a2f432cbbc703a6de4ad59759232b12466e153e
Masha-kainos <256341953+Masha-kainos@users.noreply.github.com>
2026-03-02 13:34:51 +0000

    Rebase: Remove outdated tests

--- tests/api/clients/HIVResultsApiResource.ts
+++ tests/api/clients/HIVResultsApiResource.ts
@@ -2,7 +2,6 @@ import { APIResponse } from '@playwright/test';
 import { BaseApiClient } from './BaseApiClient';
 import { API_ENDPOINTS } from '../Endpoints';
 import { HIVTestResult } from '../../test-data/HIVTestResultData';
-<<<<<<< HEAD
 import { ApiHeaders } from '../../utils/ApiRequestHelper';
 import {
   GetResultHeaders,
@@ -22,27 +21,11 @@ export class HIVResultsApiResource extends BaseApiClient {
   ): Promise<APIResponse> {
     return this.post(API_ENDPOINTS.results.base, {
       headers,
-=======
-import { headersTestResults } from '../../test-data/HeadersTestResults';
-import { createGetResultParams, createGetResultHeaders } from '../../test-data/GetResultRequestParams';
-import { ResultsObservationData } from '../../test-data/ResultsObservationData';
-
-export class HIVResultsApiResource extends BaseApiClient {
-  constructor(request: APIRequestContext) {
-    super(request);
-  }
-
-  async submitTestResults(testData: HIVTestResult | ResultsObservationData, headers: headersTestResults): Promise<APIResponse> {
-    const endpoint = API_ENDPOINTS.results.base;
-    const response = await this.post(endpoint, {
-      headers: headers,
->>>>>>> 05c18311 (local changes before pull)
       data: testData,
     });
   }
 
   async getResult(
-<<<<<<< HEAD
     params: GetResultParams,
     headers: GetResultHeaders,
   ): Promise<APIResponse> {
@@ -52,15 +35,4 @@ export class HIVResultsApiResource extends BaseApiClient {
     });
     return response;
   }
-=======
-  params: ReturnType<typeof createGetResultParams>,
-  headers: ReturnType<typeof createGetResultHeaders>
-): Promise<APIResponse> {
-  const response = await this.get(API_ENDPOINTS.results.get, {
-    params,
-    headers,
-  });
-  return response;
-}
->>>>>>> 05c18311 (local changes before pull)
 }
