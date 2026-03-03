e072c0e85812a7d26b429ad094324af32caf9d9b
Usmaan Ali <usmaan.ali5@nhs.net>
2026-03-03 12:55:37 +0000

    chore: add changes back in

--- tests/api/Endpoints.ts
+++ tests/api/Endpoints.ts
@@ -1,10 +1,18 @@
 export const API_ENDPOINTS = {
   results: {
     base: "/result",
-    get: "/results",
+  },
+  users: {
+    base: "/users",
+    list: "/users",
+    createUser: "/users",
+    getUser: (id: number) => `/users/${id}`,
   },
   order: {
     create: "/order",
     get: "/order",
+  },
+  orderStatus: {
+    update: "/test-order/status",
   },
 } as const;
