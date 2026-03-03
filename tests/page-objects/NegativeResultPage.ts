0b7e1793ed9fdfd053dee6caa2d9a4eaa4b3197e
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-03-03 11:19:20 +0100

    fix: formatting

--- tests/page-objects/NegativeResultPage.ts
+++ tests/page-objects/NegativeResultPage.ts
@@ -1,16 +1,15 @@
-import { Locator, Page } from '@playwright/test';
-import { BasePage } from './BasePage';
-import { ConfigFactory, type ConfigInterface } from '../configuration/EnvironmentConfiguration';
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
+import { ConfigFactory, type ConfigInterface } from "../configuration/EnvironmentConfiguration";
 
 export class NegativeResultPage extends BasePage {
   readonly config: ConfigInterface;
-  readonly result: Locator
+  readonly result: Locator;
 
   constructor(page: Page) {
     super(page);
     this.config = ConfigFactory.getConfig();
-    this.result = page.locator('p', { hasText: /^Negative$/ });
-
+    this.result = page.locator("p", { hasText: /^Negative$/ });
   }
 
   async navigateToOrderResult(orderId: string): Promise<void> {
