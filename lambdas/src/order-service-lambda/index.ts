56c0a0b2a2af84f12b906c6f171d472be143bdd3
billwirz1 <bill.wirz1@nhs.net>
2026-03-04 15:34:15 +0000

    chore: clarify naming and comments

--- lambdas/src/order-service-lambda/index.ts
+++ lambdas/src/order-service-lambda/index.ts
@@ -62,7 +62,7 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     });
 
     // Create patient, order, status and consent record in a single transaction.
-    // NOTE: This endpoint is not explicitly idempotent. Repeated requests may result in multiple status records for the same order; callers must not rely on uniqueness of order_status.order_uid for idempotency.
+    // ALPHA: This endpoint is not explicitly idempotent. Repeated requests may result in multiple status records for the same order; callers must not rely on uniqueness of order_status.order_uid for idempotency.
     const orderResult = await transactionService.createPatientOrderAndConsent(
       orderRequest.patient.nhsNumber,
       orderRequest.patient.birthDate,
