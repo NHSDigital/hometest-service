90077b30ae695fb1d6ee9a3e48f1b5d2563817fa
Masha-kainos <256341953+Masha-kainos@users.noreply.github.com>
2026-03-06 13:12:38 +0000

    Add tests for suppliers terms of use page

---
+++ tests/page-objects/SuppliersTermsOfUsePage.ts
@@ -0,0 +1,18 @@
+import { Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
+import { ConfigFactory, type ConfigInterface } from "../configuration/EnvironmentConfiguration";
+import { type Supplier } from "../models/TestOrder";
+
+export class SuppliersTermsOfUsePage extends BasePage {
+  readonly config: ConfigInterface;
+
+  constructor(page: Page) {
+    super(page);
+    this.config = ConfigFactory.getConfig();
+  }
+
+  async gotoSuppliersTerms(supplierInput: Supplier["supplier_name"]): Promise<void> {
+    const url = `${this.config.uiBaseUrl}/suppliers-terms-conditions?supplier=${encodeURIComponent(supplierInput)}`;
+    await this.page.goto(url);
+  }
+}
---
+++ tests/tests/accessibility/SuppliersTermsOfUseTest.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/CombinedTestFixture";
+
+test(
+  "Suppliers Terms and Conditions Page",
+  {
+    tag: ["@accessibility"],
+  },
+  async ({ suppliersTermsOfUsePage, accessibility }) => {
+    await suppliersTermsOfUsePage.gotoSuppliersTerms("SH:24");
+    await suppliersTermsOfUsePage.waitUntilPageLoad();
+
+    const accessErrors = await accessibility.runAccessibilityCheck(
+      suppliersTermsOfUsePage.page,
+      "Suppliers Terms and Conditions Page",
+    );
+    expect(accessErrors).toHaveLength(0);
+  },
+);
