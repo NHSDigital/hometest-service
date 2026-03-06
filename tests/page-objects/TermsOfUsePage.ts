1afd4600a1e125d25bc5f31292cc1e7f932cf1d8
ravikumarqa28 <ravi.kumar28@nhs.net>
2026-03-06 11:56:48 +0000

    Copilot comments Test scripts

--- tests/page-objects/TermsOfUsePage.ts
+++ tests/page-objects/TermsOfUsePage.ts
@@ -2,27 +2,27 @@ import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class TermsOfUsePage extends BasePage {
-  readonly CyberAwareLink: Locator;
-  readonly HelpAndSupportLink: Locator;
-  readonly HomeTestPrivacyPolicyLink: Locator;
+  readonly cyberAwareLink: Locator;
+  readonly helpAndSupportLink: Locator;
+  readonly homeTestPrivacyPolicyLink: Locator;
 
   constructor(page: Page) {
     super(page);
-    this.CyberAwareLink = page.getByRole("link", { name: "Cyber Aware website" });
-    this.HelpAndSupportLink = page.getByRole("link", { name: /help and support page/i }).first();
-    this.HomeTestPrivacyPolicyLink = page.getByRole("link", { name: "Hometest Privacy Policy" }).first();
+    this.cyberAwareLink = page.getByRole("link", { name: "Cyber Aware website" });
+    this.helpAndSupportLink = page.getByRole("link", { name: /help and support page/i }).first();
+    this.homeTestPrivacyPolicyLink = page.getByRole("link", { name: "Hometest Privacy Policy" }).first();
   }
 
   async clickCyberAwareLink(): Promise<void> {
-    await this.CyberAwareLink.click();
+    await this.cyberAwareLink.click();
   }
 
   async clickHelpAndSupportLink(): Promise<void> {
-    await this.HelpAndSupportLink.click();
+    await this.helpAndSupportLink.click();
   }
 
   async clickHomeTestPrivacyPolicyLink(): Promise<void> {
-    await this.HomeTestPrivacyPolicyLink.click();
+    await this.homeTestPrivacyPolicyLink.click();
   }
 
 }
