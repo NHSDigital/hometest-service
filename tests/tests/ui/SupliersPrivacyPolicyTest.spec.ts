bd964650a763a51b06378e76a776e0f50fcb6e46
Masha-kainos <256341953+Masha-kainos@users.noreply.github.com>
2026-03-10 12:11:24 +0000

    feature(543): add tests for the supplier privacy policy page

---
+++ tests/tests/ui/SupliersPrivacyPolicyTest.spec.ts
@@ -0,0 +1,66 @@
+import { TestOrderDbClient } from "../../db/TestOrderDbClient";
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/CombinedTestFixture";
+import { OrderBuilder } from "../../test-data/OrderBuilder";
+
+let orderId: string;
+let patientId: string;
+let orderId2: string;
+const dbClient = new TestOrderDbClient();
+
+test.describe("Suppliers Privacy Policy Page", () => {
+  test.beforeAll(
+    "Connect to the database and create a patient and orders with different suppliers",
+    async ({ testedUser }) => {
+      await dbClient.connect();
+
+      const result = await dbClient.createOrderWithPatientAndStatus(
+        new OrderBuilder().withUser(testedUser).build(),
+      );
+
+      orderId = result.order_uid;
+      patientId = result.patient_uid;
+
+      const order2 = await dbClient.createOrderWithPatientAndStatus(
+        new OrderBuilder()
+          .withUser(testedUser)
+          .withStatus("RECEIVED")
+          .withSupplier("SH:24")
+          .build(),
+      );
+
+      orderId2 = order2.order_uid;
+    },
+  );
+
+  const suppliers = [
+    { name: "Preventx", orderId: () => orderId },
+    { name: "SH:24", orderId: () => orderId2 },
+  ];
+
+  for (const supplier of suppliers) {
+    test(`Open Supplier Privacy Policy Page where the supplier is ${supplier.name}`, async ({
+      suppliersPrivacyPolicyPage,
+      orderStatusPage,
+    }) => {
+      await orderStatusPage.navigateToOrder(supplier.orderId());
+      await orderStatusPage.clickSuppliersPrivacyPolicyLink();
+      await suppliersPrivacyPolicyPage.waitUntilPageLoad();
+
+      const header = await suppliersPrivacyPolicyPage.getHeaderText();
+      console.log("Page header:", header);
+
+      expect(header).toBe(`${supplier.name} privacy policy`);
+    });
+  }
+
+  test.afterAll(
+    "Delete result status, order status, order, and patient records from the database and disconnect",
+    async ({ testedUser }) => {
+      await dbClient.deleteOrderStatusByUid(orderId);
+      await dbClient.deleteOrderByPatientUid(patientId);
+      await dbClient.deletePatientMapping(testedUser.nhsNumber!, testedUser.dob!);
+      await dbClient.disconnect();
+    },
+  );
+});
