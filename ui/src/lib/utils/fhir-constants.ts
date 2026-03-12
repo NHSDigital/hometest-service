c310847841d59fa1f1e2912cfacdcdc62f2483f3
lewisbirks <lewis.birks1@nhs.net>
2026-03-12 15:07:11 +0000

    fix: https part 2
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>

--- ui/src/lib/utils/fhir-constants.ts
+++ ui/src/lib/utils/fhir-constants.ts
@@ -2,7 +2,7 @@ export class FhirConstants {
   static readonly FHIR_BASE_URL = "https://fhir.hometest.nhs.uk";
 
   static readonly OBSERVATION_INTERPRETATION_SYSTEM =
-    "http://terminology.hl7.org/CodeSystem/v3-ObservationInterpretation";
+    "https://terminology.hl7.org/CodeSystem/v3-ObservationInterpretation";
   static readonly NORMAL_OBSERVATION_CODE = "N";
 
   static readonly ORDER_ID_SYSTEM = `${this.FHIR_BASE_URL}/Id/order-id`;
