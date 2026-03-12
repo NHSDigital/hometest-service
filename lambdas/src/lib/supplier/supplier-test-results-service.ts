05bd375d9c5178ffc6b4fb04e3e8ef32cdfe0a76
lewisbirks <lewis.birks1@nhs.net>
2026-03-12 14:20:37 +0000

    ci: add logging
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>

--- lambdas/src/lib/supplier/supplier-test-results-service.ts
+++ lambdas/src/lib/supplier/supplier-test-results-service.ts
@@ -23,11 +23,18 @@ export class SupplierTestResultsService {
     supplierId: string,
     correlationId: string,
   ): Promise<Bundle<Observation>> {
+    console.log(`Supplier ${supplierId} - Getting supplier config`);
+
     const serviceConfig = await this.supplierDb.getSupplierConfigBySupplierId(supplierId);
 
     if (!serviceConfig) {
       throw new Error("Missing supplier config for: " + supplierId);
     }
+
+    console.log(`Supplier ${supplierId} - Supplier config retrieved successfully`, {
+      supplierId,
+      serviceConfig: JSON.stringify(serviceConfig),
+    })
 
     const supplierAuthClient = OAuthSupplierAuthClient.fromSupplierConfig(
       this.httpClient,
@@ -35,16 +42,28 @@ export class SupplierTestResultsService {
       serviceConfig,
     );
 
+    console.log(`Supplier ${supplierId} - Fetching access token`);
+
     const accessToken = await supplierAuthClient.getAccessToken();
+
+    console.log(`Supplier ${supplierId} - Access token fetched successfully`)
 
     const resultsUrl = `${serviceConfig.serviceUrl}${serviceConfig.resultsPath}`;
     const url = new URL(resultsUrl);
     url.searchParams.append("order_uid", orderId);
 
+    console.log(`Supplier ${supplierId} - Fetching results from: ${url.toString()}`);
+
     const response = await this.httpClient.get<Bundle<Observation>>(url.toString(), {
       Authorization: `Bearer ${accessToken}`,
       Accept: "application/fhir+json",
       "X-Correlation-ID": correlationId,
+    });
+
+    console.log(`Supplier ${supplierId} - Results fetched successfully`, {
+      supplierId,
+      correlationId,
+      response: JSON.stringify(response),
     });
 
     return response;
