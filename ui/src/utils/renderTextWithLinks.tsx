f4d123a6757f464d1ca8dc1981c98e467a6d9da0
lewisbirks <lewis.birks1@nhs.net>
2026-03-13 18:16:35 +0000

    chore: reduce churn
    Signed-off-by: lewisbirks <lewis.birks1@nhs.net>

--- ui/src/utils/renderTextWithLinks.tsx
+++ ui/src/utils/renderTextWithLinks.tsx
@@ -1,6 +1,6 @@
-import {Link} from "react-router-dom";
-import type {ReactNode} from "react";
-import {OpensInNewTabLink} from "@/components/OpensInNewTabLink";
+import { Link } from "react-router-dom";
+import type { ReactNode } from "react";
+import { OpensInNewTabLink } from "@/components/OpensInNewTabLink";
 
 const BOLD = /\*\*([^*]+)\*\*/; // **bold text**
 const MARKDOWN_LINK = /\[([^\][\n]+)]\(([^()\n]+)\)/; // [display text](href)
