8f6c2a143b92db47b92ab61891823acf46cd2c0e
Carl Fagg <carlf@kainos.com>
2026-03-13 17:07:06 +0000

    refactor: moved enrichment to login-service to enusre data consistency

--- lambdas/src/login-lambda/login-service.ts
+++ lambdas/src/login-lambda/login-service.ts
@@ -1,10 +1,10 @@
-import { type JwtPayload } from 'jsonwebtoken';
-import { type INhsLoginClient } from '../lib/login/nhs-login-client';
-import { type ITokenService } from '../lib/login/token-service';
-import { type INhsTokenResponseModel } from '../lib/models/nhs-login/nhs-login-token-response-model';
-import { type INhsUserInfoResponseModel } from '../lib/models/nhs-login/nhs-login-user-info-response-model';
-import { enrichUserInfoWithTestFirstName } from '../lib/login/test-user-mapping';
-import { type LoginBody } from '.';
+import { type JwtPayload } from "jsonwebtoken";
+import { type INhsLoginClient } from "../lib/login/nhs-login-client";
+import { type ITokenService } from "../lib/login/token-service";
+import { type INhsTokenResponseModel } from "../lib/models/nhs-login/nhs-login-token-response-model";
+import { type INhsUserInfoResponseModel } from "../lib/models/nhs-login/nhs-login-user-info-response-model";
+import { enrichUserInfoWithTestFirstName } from "../lib/login/test-user-mapping";
+import { type LoginBody } from ".";
 
 // ALPHA: This file will need revisiting.
 export interface ILoginService {
@@ -35,28 +35,25 @@ export class LoginService {
 
   public async performLogin(loginBody: LoginBody): Promise<ILoginOutput> {
     try {
-      const tokenResponse: INhsTokenResponseModel =
-        await this.nhsLoginClient.getUserTokens(loginBody.code);
-      const nhsLoginIdToken = await this.tokenService.verifyToken(
-        tokenResponse.id_token
+      const tokenResponse: INhsTokenResponseModel = await this.nhsLoginClient.getUserTokens(
+        loginBody.code,
       );
+      const nhsLoginIdToken = await this.tokenService.verifyToken(tokenResponse.id_token);
       await this.tokenService.verifyToken(tokenResponse.access_token);
       const idTokenPayload = nhsLoginIdToken.payload as JwtPayload;
 
-      const userInfoResponse: INhsUserInfoResponseModel =
-        await this.nhsLoginClient.getUserInfo(tokenResponse.access_token);
+      const userInfoResponse: INhsUserInfoResponseModel = await this.nhsLoginClient.getUserInfo(
+        tokenResponse.access_token,
+      );
 
       if (idTokenPayload.sub !== userInfoResponse.sub) {
         const errorMessage =
-          'The sub claim in the user info response does not match the sub claim in the id token';
+          "The sub claim in the user info response does not match the sub claim in the id token";
         throw new Error(errorMessage);
       }
 
-      // ALPHA: Enrich user info with test data for missing given_name (temporary workaround)
-      const enrichedUserInfo = enrichUserInfoWithTestFirstName(userInfoResponse);
-
       return {
-        userInfoResponse: enrichedUserInfo,
+        userInfoResponse: userInfoResponse,
         nhsLoginAccessToken: tokenResponse.access_token,
         nhsLoginRefreshToken: tokenResponse.refresh_token,
       };
