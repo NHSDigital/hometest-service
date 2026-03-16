36da2c624d03ad599affd9846fd1a9b69afd3d1b
Przemyslaw <przemyslaw.biesek1@nhs.net>
2026-03-16 14:14:37 +0100

    fix: update TermsOfUsePage and BasePage for improved header handling and fix test references

--- tests/page-objects/TermsOfUsePage.ts
+++ tests/page-objects/TermsOfUsePage.ts
@@ -5,11 +5,13 @@ export class TermsOfUsePage extends BasePage {
   readonly cyberAwareLink: Locator;
   readonly helpAndSupportLink: Locator;
   readonly homeTestPrivacyPolicyLink: Locator;
+  readonly pageHeader: Locator;
 
   constructor(page: Page) {
     super(page);
     this.cyberAwareLink = page.getByRole("link", { name: "Cyber Aware website" });
     this.helpAndSupportLink = page.getByRole("link", { name: /help and support page/i }).first();
+    this.pageHeader = page.getByRole("heading", { name: "Hometest Terms of Use - Draft V1 January 2026" });
     this.homeTestPrivacyPolicyLink = page.getByRole("link", { name: "Hometest Privacy Policy" }).first();
   }
 
@@ -23,6 +25,9 @@ export class TermsOfUsePage extends BasePage {
 
   async clickHomeTestPrivacyPolicyLink(): Promise<void> {
     await this.homeTestPrivacyPolicyLink.click();
+  }
+    async waitUntilPageLoaded(): Promise<void> {
+    await this.pageHeader.waitFor({ state: "visible" });
   }
 
 }
