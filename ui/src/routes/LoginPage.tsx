fa10e86959d54b313307093d482d6d9096a8eca8
billwirz1 <bill.wirz1@nhs.net>
2026-03-18 16:04:21 +0000

    feat: make-ui-nhs-sandpit-url-to-be-configurable

--- ui/src/routes/LoginPage.tsx
+++ ui/src/routes/LoginPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { generateState, persistLoginCsrf } from "@/lib/auth/loginState";
-
+import { nhsLoginAuthorizeUrl } from "@/settings";
 import { useEffect } from "react";
 
 export default function RedirectPage() {
@@ -15,11 +15,9 @@ export default function RedirectPage() {
 
     // ALPHA: Improve this to use proper values and env variables.
     const nonce = Math.floor(1000 + Math.random() * 9000);
-    const callbackUrl = encodeURIComponent(
-      `${window.location.origin}/callback`,
-    );
+    const callbackUrl = encodeURIComponent(`${window.location.origin}/callback`);
     window.location.href =
-      `https://auth.sandpit.signin.nhs.uk/authorize` +
+      `${nhsLoginAuthorizeUrl}` +
       `?response_type=code` +
       `&client_id=hometest` +
       `&redirect_uri=${callbackUrl}` +
