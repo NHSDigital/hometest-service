74d66357fa14f6c90bf5ddb3509c0297515ad3e1
billwirz1 <bill.wirz1@nhs.net>
2026-03-18 16:04:21 +0000

    feat: make-ui-nhs-sandpit-url-to-be-configurable

--- ui/src/settings.ts
+++ ui/src/settings.ts
@@ -1,3 +1,5 @@
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+const nhsLoginAuthorizeUrl =
+  process.env.NEXT_PUBLIC_NHS_LOGIN_AUTHORIZE_URL ?? "https://auth.sandpit.signin.nhs.uk/authorize";
 
-export { backendUrl };
+export { backendUrl, nhsLoginAuthorizeUrl };
