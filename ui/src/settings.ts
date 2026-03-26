edfc60588a497d6290b8eeb7d5f767bc9b624904
billwirz1 <bill.wirz1@nhs.net>
2026-03-26 09:25:59 +0000

    fix: remove-dangerous-env-var-default

--- ui/src/settings.ts
+++ ui/src/settings.ts
@@ -1,6 +1,5 @@
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-const nhsLoginAuthorizeUrl =
-  process.env.NEXT_PUBLIC_NHS_LOGIN_AUTHORIZE_URL ?? "https://auth.sandpit.signin.nhs.uk/authorize";
+const nhsLoginAuthorizeUrl = process.env.NEXT_PUBLIC_NHS_LOGIN_AUTHORIZE_URL;
 const useWiremockAuth = process.env.NEXT_PUBLIC_USE_WIREMOCK_AUTH === "true";
 
 export { backendUrl, nhsLoginAuthorizeUrl, useWiremockAuth };
