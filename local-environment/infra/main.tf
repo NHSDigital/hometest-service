07dc0e19f91ee6769c4dc6736315b8dfefe85a3a
billwirz1 <bill.wirz1@nhs.net>
2026-03-26 10:17:55 +0000

    chore: re-add-localhost-fixes

--- local-environment/infra/main.tf
+++ local-environment/infra/main.tf
@@ -20,12 +20,12 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    apigateway     = "http://127.0.0.1:4566"
-    iam            = "http://127.0.0.1:4566"
-    lambda         = "http://127.0.0.1:4566"
-    s3             = "http://127.0.0.1:4566"
-    secretsmanager = "http://127.0.0.1:4566"
-    sqs            = "http://127.0.0.1:4566"
+    apigateway     = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
   }
 }
 
