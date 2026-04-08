2ebd51f8f6fa3890d3a0f646e57130e439b1c5f6
Mikolaj Miotk <mikolaj.miotk2@nhs.net>
2026-04-08 11:25:35 +0200

    Fix tests against aws env

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -1,7 +1,7 @@
 import { execFileSync } from "node:child_process";
 import * as path from "node:path";
 
-import { WireMockClient } from "./api/clients/WireMockClient";
+import { type WireMockMapping, WireMockClient } from "./api/clients/WireMockClient";
 import { AuthType, ConfigFactory } from "./configuration/EnvironmentConfiguration";
 import { CredentialsHelper } from "./utils";
 import { UserManagerFactory } from "./utils/users";
@@ -9,8 +9,53 @@ import {
   cleanupWireMockAuthState,
   configureWireMockAuthMappings,
   createWireMockAuthManifest,
+  createWireMockCatchAllAuthorizeMapping,
 } from "./utils/users/wiremockAuthMappings";
 import { createWireMockUserInfoMapping } from "./utils/users/wiremockUserInfoMapping";
+import { createSupplierOAuthTokenMapping } from "./utils/wireMockMappings/SupplierOAuthWireMockMappings";
+import { createSupplierOrderSuccessMapping } from "./utils/wireMockMappings/SupplierOrderWireMockMappings";
+
+function createOSPlacesCatchAllMapping(): WireMockMapping {
+  return {
+    priority: 100,
+    request: {
+      method: "GET",
+      urlPath: "/find",
+      queryParameters: { query: { matches: ".*" } },
+    },
+    response: {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+      jsonBody: {
+        header: {
+          uri: "http://wiremock/find",
+          query: "query=SW1A1AA",
+          offset: 0,
+          totalresults: 1,
+          format: "JSON",
+          dataset: "DPA",
+          lr: "EN",
+          maxresults: 100,
+          epoch: "95",
+          output_srs: "EPSG:27700",
+        },
+        results: [
+          {
+            DPA: {
+              UPRN: "100023336956",
+              UDPRN: "52640002",
+              ADDRESS: "10 DOWNING STREET, LONDON, SW1A 1AA",
+              BUILDING_NUMBER: "10",
+              THOROUGHFARE_NAME: "DOWNING STREET",
+              POST_TOWN: "LONDON",
+              POSTCODE: "SW1A 1AA",
+            },
+          },
+        ],
+      },
+    },
+  };
+}
 
 async function globalSetup() {
   console.log("🚀 Global setup started");
@@ -21,15 +66,28 @@ async function globalSetup() {
     cleanupWireMockAuthState();
 
     const wiremock = new WireMockClient(config.wiremockBaseUrl);
-    const manifest = createWireMockAuthManifest();
+    const manifest = createWireMockAuthManifest(config.wiremockBaseUrl);
 
     await configureWireMockAuthMappings(wiremock, manifest);
 
     for (const user of [...manifest.workerUsers, ...Object.values(manifest.specialUsers)]) {
       await wiremock.createMapping(
-        createWireMockUserInfoMapping(user, user.authContext.accessToken, user.authContext.sub),
+        createWireMockUserInfoMapping(
+          user,
+          user.authContext.accessToken,
+          user.authContext.sub,
+          config.wiremockBaseUrl,
+        ),
       );
     }
+
+    // Push catch-all /authorize fallback for browsers without login_hint
+    await wiremock.createMapping(createWireMockCatchAllAuthorizeMapping(manifest.workerUsers[0]));
+
+    // Push static supplier and OS Places stubs (wiped by resetAllMappings above)
+    await wiremock.createMapping(createSupplierOAuthTokenMapping());
+    await wiremock.createMapping(createSupplierOrderSuccessMapping());
+    await wiremock.createMapping(createOSPlacesCatchAllMapping());
 
     resetLoginLambdaCache();
   } else {
