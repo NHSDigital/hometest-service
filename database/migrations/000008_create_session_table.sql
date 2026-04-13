2f7ce4b9fd391561a456a2290a6b9c440ef4b8d0
billwirz1 <bill.wirz1@nhs.net>
2026-04-13 11:42:11 +0100

    chore: update-schema-with-internal-representations

--- database/migrations/000008_create_session_table.sql
+++ database/migrations/000008_create_session_table.sql
@@ -4,28 +4,22 @@ CREATE TABLE IF NOT EXISTS session
   session_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   refresh_token_id uuid NOT NULL,
   nhs_access_token text NOT NULL,
-  iss text NOT NULL,
-  aud text NOT NULL,
-  sub text NOT NULL,
+  issuer text NOT NULL,
+  audience text NOT NULL,
+  subject text NOT NULL,
   family_name varchar(255) NOT NULL,
   given_name varchar(255) NOT NULL,
   identity_proofing_level varchar(10) NOT NULL,
   email varchar(320) NOT NULL,
-  email_verified varchar(5) NOT NULL,
-  phone_number_verified varchar(5) NOT NULL,
-  birthdate varchar(10) NOT NULL,
+  email_verified boolean NOT NULL,
+  phone_number_verified boolean NOT NULL,
+  birth_date date NOT NULL,
   nhs_number varchar(10) NOT NULL,
   gp_ods_code varchar(20) NOT NULL,
   session_created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
   last_refresh_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
   max_expires_at timestamp with time zone NOT NULL,
   CONSTRAINT uq_session_refresh_token_id UNIQUE (refresh_token_id),
-  CONSTRAINT chk_session_email_verified CHECK (
-    email_verified IN ('true', 'false')
-  ),
-  CONSTRAINT chk_session_phone_number_verified CHECK (
-    phone_number_verified IN ('true', 'false')
-  ),
   CONSTRAINT chk_session_nhs_number_format CHECK (nhs_number ~ '^[0-9]{10}$')
 );
 
