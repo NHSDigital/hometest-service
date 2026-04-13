4ecc72967479503d4081cd84dfbdf2025dfb9798
billwirz1 <bill.wirz1@nhs.net>
2026-04-13 09:56:17 +0100

    feat: make-all-session-fields-not-null

--- database/migrations/000008_create_session_table.sql
+++ database/migrations/000008_create_session_table.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS session
   aud text NOT NULL,
   sub text NOT NULL,
   family_name varchar(255) NOT NULL,
-  given_name varchar(255),
+  given_name varchar(255) NOT NULL,
   identity_proofing_level varchar(10) NOT NULL,
-  email varchar(320),
-  email_verified varchar(5),
-  phone_number_verified varchar(5),
+  email varchar(320) NOT NULL,
+  email_verified varchar(5) NOT NULL,
+  phone_number_verified varchar(5) NOT NULL,
   birthdate varchar(10) NOT NULL,
   nhs_number varchar(10) NOT NULL,
-  gp_ods_code varchar(20),
+  gp_ods_code varchar(20) NOT NULL,
   session_start_at timestamp with time zone NOT NULL,
   last_refresh_at timestamp with time zone NOT NULL,
   max_expires_at timestamp with time zone NOT NULL,
@@ -25,10 +25,10 @@ CREATE TABLE IF NOT EXISTS session
   CONSTRAINT uq_session_refresh_token_id UNIQUE (refresh_token_id),
   CONSTRAINT chk_session_metadata_object CHECK (jsonb_typeof(metadata) = 'object'),
   CONSTRAINT chk_session_email_verified CHECK (
-    email_verified IS NULL OR email_verified IN ('true', 'false')
+    email_verified IN ('true', 'false')
   ),
   CONSTRAINT chk_session_phone_number_verified CHECK (
-    phone_number_verified IS NULL OR phone_number_verified IN ('true', 'false')
+    phone_number_verified IN ('true', 'false')
   ),
   CONSTRAINT chk_session_nhs_number_format CHECK (nhs_number ~ '^[0-9]{10}$')
 );
