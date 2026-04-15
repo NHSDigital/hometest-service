240aafe7c0a4ab3a9b12cd32df29e3258f4f1d75
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-04-15 16:02:25 +0200

    refactoring for notify service

---
+++ lambdas/src/lib/notify/services/base-notify-service.ts
@@ -0,0 +1,63 @@
+import { ConsoleCommons } from "../../commons";
+import {
+  NotificationAuditDbClient,
+  NotificationAuditStatus,
+} from "../../db/notification-audit-db-client";
+import { SQSClientInterface } from "../../sqs/sqs-client";
+import { type NotifyMessage } from "../../types/notify-message";
+
+const commons = new ConsoleCommons();
+const name = "notify-service";
+
+export interface NotifyServiceDependencies {
+  notificationAuditDbClient: NotificationAuditDbClient;
+  sqsClient: SQSClientInterface;
+  notifyMessagesQueueUrl: string;
+}
+
+export abstract class BaseNotifyService {
+  constructor(protected readonly dependencies: NotifyServiceDependencies) {}
+
+  protected async dispatchNotification(
+    notifyMessage: NotifyMessage,
+    orderId: string,
+  ): Promise<void> {
+    const { correlationId } = notifyMessage;
+    const { notificationAuditDbClient, sqsClient, notifyMessagesQueueUrl } = this.dependencies;
+
+    let sqsMessageId: string | undefined;
+
+    try {
+      const sqsResult = await sqsClient.sendMessage(
+        notifyMessagesQueueUrl,
+        JSON.stringify(notifyMessage),
+      );
+      sqsMessageId = sqsResult.messageId;
+
+      await notificationAuditDbClient.insertNotificationAuditEntry({
+        messageReference: notifyMessage.messageReference,
+        eventCode: notifyMessage.eventCode,
+        correlationId,
+        status: NotificationAuditStatus.QUEUED,
+      });
+
+      commons.logInfo(name, "Notification dispatched", {
+        correlationId,
+        orderId,
+        eventCode: notifyMessage.eventCode,
+        sqsMessageId,
+        messageReference: notifyMessage.messageReference,
+      });
+    } catch (error) {
+      commons.logError(name, "Failed to dispatch notification", {
+        correlationId,
+        orderId,
+        eventCode: notifyMessage.eventCode,
+        messageReference: notifyMessage.messageReference,
+        sqsMessageId,
+        error,
+      });
+      throw error;
+    }
+  }
+}
