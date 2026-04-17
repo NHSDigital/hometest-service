7bc90bfa66c4e74cc41fb757e774151d0b4e62ec
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-04-17 13:53:14 +0200

    added index for order status view

--- database/migrations/000010_add_latest_order_status_view.sql
+++ database/migrations/000010_add_latest_order_status_view.sql
@@ -9,5 +9,8 @@ SELECT DISTINCT ON (order_uid)
 FROM order_status
 ORDER BY order_uid ASC, created_at DESC;
 
+CREATE INDEX IF NOT EXISTS idx_order_status_order_uid_created_at ON order_status (order_uid ASC, created_at DESC);
+
 -- +goose Down
+DROP INDEX IF EXISTS idx_order_status_order_uid_created_at;
 DROP VIEW IF EXISTS latest_order_status;
