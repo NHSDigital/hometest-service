c39394605bb6cc8caffe9c7d84ffdac776667726
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-04-20 10:27:23 +0200

    update the index to include order id

--- database/migrations/000010_add_latest_order_status_view.sql
+++ database/migrations/000010_add_latest_order_status_view.sql
@@ -11,6 +11,14 @@ ORDER BY order_uid ASC, created_at DESC;
 
 CREATE INDEX IF NOT EXISTS idx_order_status_order_uid_created_at ON order_status (order_uid ASC, created_at DESC);
 
+DROP INDEX IF EXISTS idx_order_status_reminder_status_triggered_at;
+
+CREATE INDEX IF NOT EXISTS idx_order_status_reminder_status_order_uid_triggered_at
+ON order_status_reminder (status, order_uid, triggered_at);
+
 -- +goose Down
+DROP INDEX IF EXISTS idx_order_status_reminder_status_order_uid_triggered_at;
+CREATE INDEX IF NOT EXISTS idx_order_status_reminder_status_triggered_at
+ON order_status_reminder (status, triggered_at);
 DROP INDEX IF EXISTS idx_order_status_order_uid_created_at;
 DROP VIEW IF EXISTS latest_order_status;
