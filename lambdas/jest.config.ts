d6a4995355e9aeb0c72a0ff691f6697acab56b64
Jarosław Rejterada <jaroslaw.rejterada1@nhs.net>
2026-04-27 14:01:16 +0200

    fix the module mapper

--- lambdas/jest.config.ts
+++ lambdas/jest.config.ts
@@ -22,6 +22,12 @@ const config: Config = {
   ],
   moduleNameMapper: {
     "^@hometest-service/shared/(.*)$": "<rootDir>/../shared/$1",
+    "^@middy/core$": "<rootDir>/node_modules/@middy/core/index.js",
+    "^@middy/http-cors$": "<rootDir>/node_modules/@middy/http-cors/index.js",
+    "^@middy/http-error-handler$": "<rootDir>/node_modules/@middy/http-error-handler/index.js",
+    "^@middy/http-security-headers$":
+      "<rootDir>/node_modules/@middy/http-security-headers/index.js",
+    "^@middy/util$": "<rootDir>/node_modules/@middy/util/index.js",
   },
   transform: {
     "^.+\\.ts$": [
